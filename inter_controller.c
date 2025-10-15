bc3340b444ea204a8b40a90ffdccf30dc5033c93
Breno Soares <bresoa0@gmail.com>
2025-10-15 20:09:03 -0300

    Adicionando comunicacao com FIFO

---
+++ inter_controller.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FIFO_PATH "/tmp/kernel_fifo"
+
+int main() {
+    srand(time(NULL));
+
+    int fd = open(FIFO_PATH, O_WRONLY);
+    if (fd < 0) {
+        perror("[InterControllerSim] Erro ao abrir FIFO");
+        exit(1);
+    }
+
+    printf("[InterControllerSim] Iniciado...\n");
+
+    while (1) {
+        usleep(500000);
+
+        int irq = 0;
+        write(fd, &irq, sizeof(int));
+        printf("[InterControllerSim] IRQ0 (TimeSlice)\n");
+
+        if ((rand() % 100) < 10) {
+            irq = 1;
+            write(fd, &irq, sizeof(int));
+            printf("[InterControllerSim] IRQ1 (D1 terminado)\n");
+        }
+
+        if ((rand() % 100) < 10) {
+            irq = 2;
+            write(fd, &irq, sizeof(int));
+            printf("[InterControllerSim] IRQ2 (D2 terminado)\n");
+        }
+
+        fflush(stdout);
+    }
+
+    close(fd);
+    return 0;
+}
--- kernel.c
+++ kernel.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define NPROC 5
 #define TIMESLICE 1
+#define FIFO_PATH "/tmp/kernel_fifo"
 
 pid_t apps[NPROC];
 int current = 0;
@@ -18,6 +20,9 @@ void escalona_proximo() {
 }
 
 int main() {
+    mkfifo(FIFO_PATH, 0666); // Cria FIFO para comunicação com InterController
+
+    // Criação dos processos de aplicação
     for (int i = 0; i < NPROC; i++) {
         pid_t pid = fork();
         if (pid == 0) {
@@ -30,12 +35,31 @@ int main() {
         }
     }
 
+    // Inicia primeiro processo
     sleep(1);
     kill(apps[current], SIGCONT);
 
+    int fd = open(FIFO_PATH, O_RDONLY);
+    int irq;
+
     while (1) {
-        sleep(TIMESLICE);
-        escalona_proximo();
+        if (read(fd, &irq, sizeof(int)) > 0) {
+            switch (irq) {
+                case 0:
+                    printf("[KernelSim] IRQ0 recebido: Troca de processo.\n");
+                    escalona_proximo();
+                    break;
+                case 1:
+                    printf("[KernelSim] IRQ1 recebido: operação em D1 terminou.\n");
+                    break;
+                case 2:
+                    printf("[KernelSim] IRQ2 recebido: operação em D2 terminou.\n");
+                    break;
+            }
+        }
     }
+
+    close(fd);
+    unlink(FIFO_PATH);
     return 0;
 }
