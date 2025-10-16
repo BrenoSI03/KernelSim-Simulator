dccee4a3060a691d840a72fa3ed9c68b76064616
Breno Soares <bresoa0@gmail.com>
2025-10-16 03:35:05 -0300

    Adicionando o contador de programas

--- app.c
+++ app.c
@@ -9,38 +9,54 @@
 #define FIFO_PATH "/tmp/kernel_fifo"
 #define MAX 10
 
+int pc = 0;
+int acessos_D1 = 0;
+int acessos_D2 = 0;
+int dispositivo_atual = 0;
+char operacao_atual = '-';
+
 void sys_call(int dispositivo) {
     int fd = open(FIFO_PATH, O_WRONLY | O_NONBLOCK);
     if (fd < 0) {
         perror("[App] Erro ao abrir FIFO");
-        return; // não sai do processo, apenas pula o envio
+        return;
     }
 
-    int msg = 10 + dispositivo; // 11 -> D1, 12 -> D2
+    int msg = 10 + dispositivo;
     write(fd, &msg, sizeof(int));
-
-    printf("[App %d] -> Bloqueando por D%d (enviando msg=%d)\n",
-           getpid(), dispositivo, msg);
-
     close(fd);
 
-    kill(getpid(), SIGSTOP);  // pausa o próprio processo
+    dispositivo_atual = dispositivo;
+    int op = rand() % 3;
+    if (op == 0) operacao_atual = 'R';
+    else if (op == 1) operacao_atual = 'W';
+    else operacao_atual = 'X';
+
+    if (dispositivo == 1) acessos_D1++;
+    else acessos_D2++;
+
+    printf("[App %d] -> Bloqueando por D%d (Operação %c, PC=%d)\n",
+           getpid(), dispositivo, operacao_atual, pc);
+
+    kill(getpid(), SIGSTOP);
 }
 
 int main() {
     srand(time(NULL) ^ getpid());
-    int pc = 0;
     printf("[App %d] iniciado.\n", getpid());
 
     while (pc < MAX) {
         sleep(1);
+
         if (rand() % 100 < 15) {
             int dispositivo = (rand() % 2) + 1;
             sys_call(dispositivo);
         }
+
         pc++;
     }
 
-    printf("[App %d] terminou.\n", getpid());
+    printf("[App %d] terminou. PC final=%d | D1=%d | D2=%d\n",
+           getpid(), pc, acessos_D1, acessos_D2);
     return 0;
 }
